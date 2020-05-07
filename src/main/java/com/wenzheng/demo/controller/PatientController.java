4f14ad40bf12370a67b2c8f68ff1adfa7fa082a9
logique233 <2219422983@qq.com>
2020-05-07 20:48:13 +0800

    添加cookie 完成
    下一步 添加 登录注册页面

--- src/main/java/com/wenzheng/demo/controller/PatientController.java
+++ src/main/java/com/wenzheng/demo/controller/PatientController.java
@@ -7,12 +7,15 @@ import com.wenzheng.demo.util.R;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 
 @Controller
@@ -39,7 +42,7 @@ public class PatientController {
     @RequestMapping("/login")
     @PostMapping
     @ResponseBody
-    public R<User> patentLogin(String name, String password){
+    public ResponseEntity<R<User>> patentLogin(String name, String password,HttpServletResponse response){
         //TODO 患者登录
         R<User> result = new R<User>();
         try {
@@ -48,16 +51,27 @@ public class PatientController {
             if (user != null) {
                 result.code=200;
                 result.data = user;
+                Cookie cookieID = new Cookie("ID", user.getId());
+                Cookie cookieStatus = new Cookie("STATUS", user.getStatus());
+                cookieID.setMaxAge(7 * 24 * 60 * 60);
+                cookieID.setHttpOnly(true);
+                cookieID.setPath("/");
+                cookieStatus.setMaxAge(7 * 24 * 60 * 60);
+                cookieStatus.setHttpOnly(true);
+                cookieStatus.setPath("/");
+                response.addCookie(cookieID);
+                response.addCookie(cookieStatus);
+
             }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return result;
+        return new ResponseEntity<R<User>>(result, HttpStatus.OK);
     }
 
 
-    @RequestMapping("/index")
+    @RequestMapping({"/index","/"})
     @PostMapping
     public String patentIndex(){
         //TODO 患者登录
@@ -85,30 +99,42 @@ public class PatientController {
     @RequestMapping("/queryStatus")
     @GetMapping
     @ResponseBody
-    public R<String> patirntQuaryStatus(String ID){
+    public ResponseEntity<R<String>> patirntQuaryStatus(String ID, HttpServletResponse response){
         //TODO 患者查询知识图谱
         R<String> result = new R<String>();
         try {
             String status = patirntService.GetStatus(ID);
             result.code = 200;
             result.data = status;
+            Cookie cookie = new Cookie("STATUS", status);
+            cookie.setMaxAge(7 * 24 * 60 * 60);
+//            cookie.setSecure(true);
+            cookie.setHttpOnly(true);
+            cookie.setPath("/");
+            response.addCookie(cookie);
         } catch (Exception e) {
             e.printStackTrace();
             result.code = 500;
         }
-        return result;
+        return new ResponseEntity<R<String>>(result, HttpStatus.OK);
     }
 
     @RequestMapping("/SetStatus")
     @GetMapping
     @ResponseBody
-    public R<String> patirntSetStatus(String ID,String Status){
+    public R<String> patirntSetStatus(String ID,String Status,HttpServletResponse response){
         //TODO 患者查询知识图谱
         R<String> result = new R<String>();
         try {
             if (patirntService.SetStatus(ID, Status, new Date())>0);
             {
                 result.code = 200;
+                Cookie cookie = new Cookie("STATUS", Status);
+                cookie.setMaxAge(7 * 24 * 60 * 60);
+//            cookie.setSecure(true);
+                cookie.setHttpOnly(true);
+                cookie.setPath("/");
+                response.addCookie(cookie);
             }
         } catch (Exception e) {
             e.printStackTrace();
